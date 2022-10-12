4548d9532190ae1699352055ddc694e72191f0df
Beyza Uyanıksoy <beyzauyanik57@hotmail.com>
2022-10-12 20:59:01 +0300

    fooditem widgets oluşturuldu

--- lib/widgets/fooditem.dart
+++ lib/widgets/fooditem.dart
@@ -5,7 +5,17 @@ import 'package:flutter/src/widgets/framework.dart';
 
 class FoodItem extends StatefulWidget {
   final String imageUrl;
-  const FoodItem({super.key, required this.imageUrl});
+  final String foodName;
+  final String foodPoint;
+  final String mins;
+  final String km;
+  const FoodItem(
+      {super.key,
+      required this.imageUrl,
+      required this.foodName,
+      required this.foodPoint,
+      required this.mins,
+      required this.km});
 
   @override
   State<FoodItem> createState() => _FoodItemState();
@@ -18,7 +28,14 @@ class _FoodItemState extends State<FoodItem> {
       margin: EdgeInsets.all(5),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10),
-        color: Colors.blue,
+        color: Colors.white,
+        boxShadow: const [
+          BoxShadow(
+              color: Color(0x29000000),
+              offset: Offset(2, 3),
+              blurRadius: 6,
+              spreadRadius: 0)
+        ],
       ),
       width: 360,
       height: 120,
@@ -29,7 +46,7 @@ class _FoodItemState extends State<FoodItem> {
             width: 120,
             decoration: BoxDecoration(
                 color: Colors.amber, borderRadius: BorderRadius.circular(10)),
-            child: Image.asset(
+            child: Image.network(
               widget.imageUrl,
               fit: BoxFit.cover,
             ),
@@ -40,16 +57,50 @@ class _FoodItemState extends State<FoodItem> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  'Burger Menü Adı',
+                  widget.foodName,
                   style: TextStyle(
                       color: Color(0xff010E16),
                       fontWeight: FontWeight.w500,
                       fontSize: 13.57),
                 ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 6.3, bottom: 22.78),
+                  child: Row(
+                    children: [
+                      Image.asset("assets/star.png"),
+                      Text(
+                        widget.foodPoint,
+                        style: TextStyle(
+                          color: Color(0xffB3B3B3),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
                 Row(
                   children: [
-                    Image.asset("assets/star.png"),
-                    Text('4.5',style: TextStyle(color: Color(0xffB3B3B3),),)
+                    Image.asset(
+                      'assets/carbon_time.png',
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 6.79, right: 15.17),
+                      child: Text(
+                        widget.mins,
+                        style: TextStyle(
+                            color: Color(0xffB3B3B3),
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                    Image.asset('assets/dot.png'),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 6.79),
+                      child: Text(
+                        widget.km,
+                        style: TextStyle(
+                            color: Color(0xffB3B3B3),
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ),
                   ],
                 )
               ],
